b955606d8191d8994a9a4ab7f4e8623c5f0a5836
Sukhrob <sukhrobtokhirov006@gmail.com>
2024-07-28 23:39:39 +0500

    AuthService complete

--- src/main/java/sukhrob/developer/rest_api/repo/UserRepository.java
+++ src/main/java/sukhrob/developer/rest_api/repo/UserRepository.java
@@ -3,8 +3,11 @@ package sukhrob.developer.rest_api.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sukhrob.developer.rest_api.entities.User;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
+
+    Optional<User> findByUsername(String phoneNumber);
 }
--- src/main/java/sukhrob/developer/rest_api/repo/VerificationCodeRepository.java
+++ src/main/java/sukhrob/developer/rest_api/repo/VerificationCodeRepository.java
@@ -1,13 +1,29 @@
 package sukhrob.developer.rest_api.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import sukhrob.developer.rest_api.entities.VerificationCode;
 
 import java.sql.Timestamp;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface VerificationCodeRepository extends JpaRepository<VerificationCode, UUID> {
 
     Long countAllByPhoneNumberAndCreatedAtAfter(String phoneNumber, Timestamp timestamp);
+
+    @Query(value = """
+            select *
+            from verification_code vc
+            where phone_number = :phoneNumber
+              and code = :code
+              and created_at > :pastTime
+              and confirmed = false
+            order by created_at desc
+            limit 1""",nativeQuery = true)
+    Optional<VerificationCode> getByCondition(@Param("phoneNumber") String phoneNumber,
+                                              @Param("code") String code,
+                                              @Param("pastTime") Timestamp pastTime);
 
 }
